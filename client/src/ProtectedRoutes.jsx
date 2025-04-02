d147baeb19e80fd9506105a4c6357ff349d78662
Ayush Kumar <shyau9847@gmail.com>
2025-04-02 17:55:04 +0530

    change on api call

--- client/src/ProtectedRoutes.jsx
+++ client/src/ProtectedRoutes.jsx
@@ -3,9 +3,13 @@ import { useSelector } from 'react-redux';
 import { Navigate, Outlet } from 'react-router-dom';
 
 function ProtectedRoutes() {
-   const isAuthenticated = localStorage.getItem("token"); // Change this based on your auth logic
+    const { user, loading } = useSelector((state) => state.auth);
+    // console.log(user)
+    if (loading) {
+        return <div>Loading...</div>;
+    }
 
-   return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
+    return user ? <Outlet /> : <Navigate to="/login" replace />;
 }
 
 export default ProtectedRoutes
