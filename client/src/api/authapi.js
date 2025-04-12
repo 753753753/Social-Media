4a3d1dcd81dadfbda5d3d4e0c7c667130af48e6c
Ayush Kumar <shyau9847@gmail.com>
2025-04-12 20:35:35 +0530

    change

--- client/src/api/authapi.js
+++ client/src/api/authapi.js
@@ -1,5 +1,5 @@
-const BASE_URL = "http://localhost:3000";
-// const BASE_URL = "https://social-media-backend-alpha.vercel.app"; 
+// const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://social-media-backend-alpha.vercel.app"; 
 
 // Save token in localStorage
 const saveToken = (token) => {
